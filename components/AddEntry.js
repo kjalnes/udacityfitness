5ec9a8897883c21d726903f716e06d8b457ff616
kjalnes <kris.alnes@gmail.com>
2017-12-12 14:04:55 -0500

    Methods added to AddEntry component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,8 +3,41 @@ import { View, Text } from 'react-native';
 import { getMetricMetaInfo } from '../utils/helpers';
 
 class AddEntry extends Component {
-    constructor() {
-      super()
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0
+    }
+
+    increment = (metric) => {
+        const { max, step } = getMetricMetaInfo(metric);
+
+        this.setState((state) => {
+            const count = state[metric] + step;
+
+            return {
+                ...state,
+                [metric]: count > max ? max : count
+            }
+        })
+    }
+
+    decrement = (metric) => {
+        this.setState( (state) => {
+            count = state[metric] - step
+        })
+        return {
+            ...state,
+            [metric]: count < 0 ? 0 : count
+        }
+    }
+
+    slide = (metric, value) => {
+        this.setState(() => ({
+            [metric]: value
+        }))
     }
 
     render() {
