3673750ff349e6f5eac464d8de48befb99d6d820
kjalnes <kris.alnes@gmail.com>
2017-12-12 14:53:18 -0500

    UdaciSteppers and UdaciSlider components added

---
+++ components/UdaciSlider.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function UdaciSlider() {
+    return (
+        <View>
+            <Text>UdaciSlider</Text>
+        </View>
+    )
+}
